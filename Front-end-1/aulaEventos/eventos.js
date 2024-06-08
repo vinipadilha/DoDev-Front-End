ff606784eb0ee8ce3f694f1ce4aaa134a244e40f
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-06-07 21:42:40 -0300

    mexendo com eventos de imagens

--- Front-end-1/aulaEventos/eventos.js
+++ Front-end-1/aulaEventos/eventos.js
@@ -3,13 +3,16 @@
 
 
 function executar(event){
-console.log("Olá")
-console.log(event)
+    console.log("Olá")
+    console.log(screenY)
+    console.log(event)
+
 }
 
 
 document.getElementById('form',  ).addEventListener("submit", (e) =>{
     alert("Recebido")
-
 })
+
+
 
