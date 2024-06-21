a41b9651190d3b8f2c8e00713c50bd1041b30f25
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-06-20 21:18:01 -0300

    .

--- Front-end-1/Pratica-02/pratica.js
+++ Front-end-1/Pratica-02/pratica.js
@@ -1,6 +1,19 @@
 const form = document.getElementById("form")
 
-form.addEventListener("submit", )
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+    const name = document.getElementById("name").value
+    const address = document.getElementById("address").value
+    const open = document.getElementById("open").value
+    const close = document.getElementById("close").value
+
+    console.log('Nome da loja:', name);
+    console.log('Endereço:', address);
+    console.log('Horário de abertura:', open);
+    console.log('Horário de fechamento:', close);
+
+
+})
 
 
 
